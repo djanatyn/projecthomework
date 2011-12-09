1cad38171fee61c62ba7570b82e1e7418bf983c2
Jonathan <djanatyn@gmail.com>
2011-12-09 06:42:46 -0500

    final commit before school

--- start.pl
+++ start.pl
@@ -13,30 +13,75 @@ set 'warnings'    => 1;
 set 'access_log'  => 1;
 
 session 'message'  => '';
+session 'previous' => '/';
 
 my %users;
-for my $user (<users/*>) {
-  my $pass = read_file("$user/pass");
+sub reload {
+  for my $user (<users/*>) {
+    my $pass = read_file("$user/pass");
 
-  my @todo;
-  my @done;
+    my @todo;
+    my @done;
 
-  open(TASKS,"$user/tasks");
-  for my $task (<TASKS>) {
-    if ($task =~ /^todo: (.+)/) {
-      push @todo, $1;
+    open(TASKS,"$user/tasks");
+    for my $task (<TASKS>) {
+      if ($task =~ /^todo: (.+)/) {
+	push @todo, $1;
+      };
+      if ($task =~ /^done: (.+)/) {
+	push @done, $1;
+      };
     };
-    if ($task =~ /^done: (.+)/) {
-      push @done, $1;
+
+    $user =~ s/users\///;
+
+    $users{$user}->{pass} = $pass;
+
+    $users{$user}->{done} = \@done;
+    $users{$user}->{todo} = \@todo;
+  };
+};
+
+reload;
+
+# sub toggle {
+#   my ($task, $executor) = shift;
+#   open(TASKS,"users/$executor/tasks");
+#   my @buffer;
+#   for (<TASKS>) {
+#     if (/$task/) {
+#       if (/^todo: /) { push @buffer, "done: $task"; };
+#       if (/^done: /) { push @buffer, "todo: $task"; }
+#     } else {
+#       push @buffer, $_;
+#     }
+#   }
+#   close(TASKS); open(TASKS,">users/executor/tasks");
+#   for (@buffer) {
+#     print TASKS $_;
+#   }
+
+#   close(TASKS);
+# };
+
+sub purge {
+  my $executor = shift;
+  open(TASKS,"users/$executor/tasks");
+
+  my @buffer;
+
+  for (<TASKS>) {
+    if (/^todo: /) {
+      push @buffer, $_;
     };
   };
 
-  $user =~ s/users\///;
+  close(TASKS);
 
-  $users{$user}->{pass} = $pass;
-
-  $users{$user}->{done} = \@done;
-  $users{$user}->{todo} = \@todo;
+  open(TASKS,">users/$executor/tasks");
+  for my $line (@buffer) {
+    print TASKS $line;
+  };
 };
 
 sub getMessage {
@@ -47,11 +92,10 @@ sub getMessage {
 sub setMessage {
   session 'message' => shift; };
 
-any ['get', 'post'] => '/' => sub {
+get '/' => sub {
+  session 'previous' => '/';
   template 'index.tt', {
     'message' => getMessage(),
-    todo => $users{params->{name}}->{todo},
-    done => $users{params->{name}}->{done},
   };
 };
 
@@ -71,26 +115,48 @@ post '/login' => sub {
 
     setMessage('created new account "' . param('user') . '".');
   };
-  redirect '/';
+  redirect session('previous');
+};
+
+get '/purge' => sub {
+  purge(session('username'));
+  setMessage("purged tasks");
+  redirect session('previous');
+};
+
+post '/add' => sub {
+  write_file('./users/' . session('username') . '/tasks', {append => 1}, "\ntodo: " . param('task'));
+  setMessage('added task "' . param('task') . '".');
+  reload;
+  redirect session('previous');
 };
 
 get '/logout' => sub {
   session->destroy;
   setMessage("logged out successfully");
-  redirect '/';
+  redirect session('previous');
 };
 
 get '/users/:name' => sub {
-  if (exists $users{params->{name}}->{pass}) {
+  if (exists $users{params->{name}}) {
+    session 'previous' => '/users/' . params->{name};
     template 'user.tt', {
       user => params->{name},
+      message => getMessage(),
       todo => $users{params->{name}}->{todo},
       done => $users{params->{name}}->{done},
     };
   } else {
     setMessage("no such user");
-    redirect '/';
+    redirect session('previous');
   };
 };
+
+# get '/toggle/:task' => sub {
+#   toggle(params->{task},session('username'));
+#   reload;
+#   setMessage('task "' . param('task') . '" toggled.');
+#   redirect session('previous');
+# };
 
 dance;
